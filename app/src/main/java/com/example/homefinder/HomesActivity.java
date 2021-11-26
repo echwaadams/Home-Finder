4cb4ee8d02851ae2e8646bd72f90a4f483053fdb
echwaadams <adamsechwa18@gmail.com>
2021-11-26 13:32:32 +0300

    created location activity

---
+++ app/src/main/java/com/example/homefinder/HomesActivity.java
@@ -0,0 +1,14 @@
+package com.example.homefinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homes);
+    }
+}
