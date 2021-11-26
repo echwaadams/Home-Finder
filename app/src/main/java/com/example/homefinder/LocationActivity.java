882d769c7f508d2096f259a4505bc679a4c6215d
echwaadams <adamsechwa18@gmail.com>
2021-11-26 13:30:44 +0300

    implemented location activity to gather user input

--- app/src/main/java/com/example/homefinder/LocationActivity.java
+++ app/src/main/java/com/example/homefinder/LocationActivity.java
@@ -2,13 +2,41 @@ package com.example.homefinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LocationActivity extends AppCompatActivity {
+    @BindView(R.id.locationTextView)
+    TextView mLocationTextView;
+    @BindView(R.id.locationEditText)
+    EditText mLocationEditText;
+    @BindView(R.id.findHomesButton)
+    Button mFindHomesButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location);
+        //binding views
+        ButterKnife.bind(this);
+
+        //setting clicklistener
+        mFindHomesButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // user input
+                String homes = mLocationEditText.getText().toString();
+                Intent intent = new Intent(LocationActivity.this, HomesActivity.class);
+                intent.putExtra("homes",homes);
+                startActivity(intent);
+            }
+        });
     }
 }
