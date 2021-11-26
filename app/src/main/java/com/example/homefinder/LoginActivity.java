d74c69b363e40aa17dc8057bfaa317223d456ce3
echwaadams <adamsechwa18@gmail.com>
2021-11-26 13:18:53 +0300

    add login activity for user to login

--- app/src/main/java/com/example/homefinder/LoginActivity.java
+++ app/src/main/java/com/example/homefinder/LoginActivity.java
@@ -2,13 +2,39 @@ package com.example.homefinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+    @BindView(R.id.loginTextView)
+    TextView mLoginTextView;
+    @BindView(R.id.userNameEditText)
+    EditText mUserNameEditText;
+    @BindView(R.id.passwordEditText) EditText mPasswordEditText;
+    @BindView(R.id.loginButton)
+    Button mLoginButton;
+    @BindView(R.id.bottomTextView) TextView mBottomTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //Binding views
+        ButterKnife.bind(this);
+
+        mLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, LocationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
