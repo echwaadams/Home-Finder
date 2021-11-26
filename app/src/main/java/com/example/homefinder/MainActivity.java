323b2d68b66eac59f6a9697309708abf02683a45
echwaadams <adamsechwa18@gmail.com>
2021-11-26 12:42:19 +0300

    add signup activity for user account creation

--- app/src/main/java/com/example/homefinder/MainActivity.java
+++ app/src/main/java/com/example/homefinder/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.homefinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.startButton)
+    Button mStartButton;
+    @BindView(R.id.textView)
+    TextView mTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Binding views
+        ButterKnife.bind(this);
+
+        // setting clickListener
+        mStartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //passing intent between activities
+                Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/homefinder/SignupActivity.java
@@ -0,0 +1,43 @@
+package com.example.homefinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class SignupActivity extends AppCompatActivity {
+    @BindView(R.id.signupTextView)
+    TextView mSignupTextView;
+    @BindView(R.id.signupButton)
+    Button mSignupButton;
+    @BindView(R.id.passwordEditText)
+    EditText mPasswordEditText;
+    @BindView(R.id.textView) TextView mTextView;
+    @BindView(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @BindView(R.id.userNameEditText) EditText mUserNameEditText;
+    @BindView(R.id.bottomTextView) TextView mBottomTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+        //Binding views
+        ButterKnife.bind(this);
+        //setting clickListener
+        mSignupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //passing intent to login activity
+                Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
