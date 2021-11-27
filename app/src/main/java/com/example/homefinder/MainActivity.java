db5ea751554786c2f2287f32d4a0b3a9c875cda5
echwaadams <adamsechwa18@gmail.com>
2021-11-28 00:49:32 +0300

    changed launcher screen textview to white color

--- app/src/main/java/com/example/homefinder/MainActivity.java
+++ app/src/main/java/com/example/homefinder/MainActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.material.button.MaterialButton;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         mStartButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Log.e("MainActivity","expected errors");
                 //passing intent between activities
                 Intent intent = new Intent(MainActivity.this, SignupActivity.class);
                 startActivity(intent);
